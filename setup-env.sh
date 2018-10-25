1a82d8a798b6b3a10a9df04a9baa226147825ee4
Shawn Song <mr.song.shawn@gmail.com>
2018-10-25 22:43:09 +1100

    moved kube-dns to addons

--- setup-env.sh
+++ setup-env.sh
@@ -5,8 +5,8 @@ sudo cp env.sh /usr/k8s/bin
 sudo chown -R $USER:$USER /user/k8s/bin
 
 cat <<EOF | sudo tee -a /etc/hosts
-192.168.1.102   etcd1
-192.168.1.101   k8s-api etcd3
-192.168.1.103   etcd2
+192.168.1.102   etcd1 master2
+192.168.1.101   etcd3 master1
+192.168.1.103   etcd2 master3
 192.168.1.201   k8s-api.virtual.local
 EOF
